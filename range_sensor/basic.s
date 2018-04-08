cc27c47d1b0423a4b32f42cd2b851bc75828aa2b
Luke Volpatti <luke.volpatti@gmail.com>
2018-04-08 18:30:32 -0400

    working range sensor code

---
+++ range_sensor/basic.s
@@ -0,0 +1,83 @@
+.include "../m328Pdef.inc"
+
+jmp reset
+
+; ADC handler interrupt vector
+.org 0x002A
+jmp adc_handler
+
+;---------------;
+; RESET ROUTINE ;
+;---------------;
+reset:	; Stack initialization
+	ldi r16, low(RAMEND)
+	out spl, r16
+	ldi r16, high(RAMEND)
+	out sph, r16
+
+	; Setting inputs and outputs
+	sbi PORTC, 2	; Enable ACD2
+	sbi DDRC, 0	; PORTC0 will be LED
+	sbi DDRC, 1	; PORTC1 is testing LED
+
+	; A status register to see if there's been an interrupt
+	ldi r19, 0
+
+;--------------;
+; MAIN ROUTINE ;
+;--------------;
+
+adc:
+	ldi r16, 0
+	sts PRR, r16
+	; Configuring ADC
+	ldi r16, 0b11001111	; Enable interrupts, prescale 128
+	sts ADCSRA, r16
+	ldi r16, 0b00100010	; External reference voltage,
+				; left adjust, ADC2
+	sts ADMUX, r16
+
+	ldi r19, 0
+
+	sei		; Enable global interrupts
+
+loop:
+	cpi r19, 0
+	breq loop
+	jmp adc
+
+
+;-----;
+; ISR ;
+;-----;
+adc_handler:
+	;ldi r16, 0b00000010
+	;out portc, r16
+	;sbi portc, 1
+	; Conversion is complete. Now need to see what the data register hold.
+	; Number in the data registers is Vin*1024/Vref. In our case, we want
+	; to see if that value ever dips below 200. 
+	lds r16, ADCL 	; Lower two bits of reading
+	lds r17, ADCH	; Upper eight bits of reading
+	lsr r17
+	lsr r17
+	lsr r17
+	lsr r17
+	lsr r17
+	lsr r17
+	;out portc, r17
+	cpi r17, 0b00000001	; Compare r17 to 265
+	brlt led_off		; Too far; turn led off
+
+	ldi r16, 1
+	out PORTC, r16		; turn on LED
+	jmp end_adc_handler
+
+led_off:
+	ldi r16, 0		; turn off LED
+	out PORTC, r16
+	jmp end_adc_handler
+
+end_adc_handler:
+	ldi r19, 1
+	reti
